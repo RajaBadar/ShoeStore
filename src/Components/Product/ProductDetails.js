462e607d0838087becb78e993b92cf076bd9995e
RajaBadar <60626675+RajaBadar@users.noreply.github.com>
2022-11-03 13:52:18 +0500

    update

--- src/Components/Product/ProductDetails.js
+++ src/Components/Product/ProductDetails.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-import { useParams } from 'react-router';
+import { useParams } from "react-router";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
@@ -8,85 +8,88 @@ import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
 
 const ProductDetails = () => {
- 
- 
-  const {id} = useParams()
-  
-  const [product,setProduct] = useState()
-  
+  const { id } = useParams();
+
+  const [product, setProduct] = useState();
+
   useEffect(() => {
     fetchData();
     // eslint-disable-next-line
   }, [setProduct]);
 
-  const fetchData = () => {
-    
-      fetch(`https://dummyjson.com/products/category/mens-shoes/`)
+  const fetchData = async () => {
+    fetch(`https://dummyjson.com/products/category/mens-shoes/`)
       .then((res) => res.json())
-      .then((data) => {
-        const newProduct = data.products.find((data)=> data.id === parseInt(id))
-        if(newProduct){
-        setProduct(newProduct)
-      }
-    }  )
+      .then(async (data) => {
+        const newProduct = await data.products.find(
+          (items) => items.id === parseInt(id)
+        );
         
-       
-      
+          setProduct(newProduct)
+        
+
+        
+      });
   };
-  const {thumbnail,description,discountPercentage,title,stock,price,brand,category,rating} = product
+  if(product){
+  const {
+    thumbnail,
+    description,
+    discountPercentage,
+    title,
+    stock,
+    price,
+    brand,
+    category,
+    rating,
+  } = product;
 
-  
-  
-
-   return (
-      <div >
-         <Card
-          key={id}
-          sx={{ maxWidth: 345, margin: 10, display: "inline-grid" }}
-        >
-          
-          <CardActionArea>
-            <CardMedia
-              component='img'
-              height='345'
-              image={thumbnail}
-              alt='Shoe'
-            />
-          <Typography gutterBottom variant='h4' component='div'>
-                {title}
-              </Typography>
-            <CardContent>
-              <Typography gutterBottom variant='h4' component='div'>
-                {brand}
-              </Typography>
-              <Typography gutterBottom variant='h4' component='div'>
-                {category}
-              </Typography>
-              <Typography gutterBottom variant='p' component='div'>
-                {description}
-              </Typography>
-              <Typography gutterBottom variant='h5' component='div'>
-                Discount: {discountPercentage}%
-              </Typography>
-              <Typography gutterBottom variant='h5' component='div'>
-                Available: {stock} pairs
-              </Typography>
-              <Typography variant='h3' color='text.secondary'>
-                Price : ${price}
-              </Typography>
-              <Typography variant='h6' color='text.secondary'>
-                Ratings : {rating}/5
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-      </div>
-     )
-  
+  return (
+    <div>
       
+      <Card key={id} sx={{ maxWidth: 345, margin: 10, display: "inline-grid" }}>
+        <CardActionArea>
+          <CardMedia
+            component='img'
+            height='345'
+            image={thumbnail}
+            alt='Shoe'
+          />
+          <Typography gutterBottom variant='h4' component='div'>
+            {title}
+          </Typography>
+          <CardContent>
+            <Typography gutterBottom variant='h4' component='div'>
+              {brand}
+            </Typography>
+            <Typography gutterBottom variant='h4' component='div'>
+              {category}
+            </Typography>
+            <Typography gutterBottom variant='p' component='div'>
+              {description}
+            </Typography>
+            <Typography gutterBottom variant='h5' component='div'>
+              Discount: {discountPercentage}%
+            </Typography>
+            <Typography gutterBottom variant='h5' component='div'>
+              Available: {stock} pairs
+            </Typography>
+            <Typography variant='h3' color='text.secondary'>
+              Price : ${price}
+            </Typography>
+            <Typography variant='h6' color='text.secondary'>
+              Ratings : {rating}/5
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </div>
+  );
+}else{
+  return(
+    <div>loading</div>
+  )}
 }
 
 
-
-
-export default  ProductDetails
+export default ProductDetails;
